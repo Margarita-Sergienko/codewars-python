f95e0bd45fc0cba48708ab8823484117687ecf31
Rita Sergienko <72668824+Margarita-Sergienko@users.noreply.github.com>
2020-11-19 22:50:22 -0800

    add Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right

---
+++ 8 kyu/Exclamation marks series #6 Remove n exclamation marks in the sentence from left to right.py
@@ -0,0 +1,19 @@
+# 8 kyu
+# Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+# https://www.codewars.com/kata/57faf7275c991027af000679
+# Remove n exclamation marks in the sentence from left to right. n is positive integer.
+# Examples
+# remove("Hi!",1) === "Hi"
+# remove("Hi!",100) === "Hi"
+# remove("Hi!!!",1) === "Hi!!"
+# remove("Hi!!!",100) === "Hi"
+# remove("!Hi",1) === "Hi"
+# remove("!Hi!",1) === "Hi!"
+# remove("!Hi!",100) === "Hi"
+# remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+# remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+def remove(s, n):
+    return s.replace("!", "", n)
