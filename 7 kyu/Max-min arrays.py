6b6dcce90e4f49eed0f96d8aaadfe8430c6bfa5a
Rita Sergienko <72668824+Margarita-Sergienko@users.noreply.github.com>
2020-11-29 22:56:44 -0800

    add Max-min arrays

---
+++ 7 kyu/Max-min arrays.py
@@ -0,0 +1,17 @@
+# 7 kyu
+# Max-min arrays
+# https://www.codewars.com/kata/5a090c4e697598d0b9000004
+# In this Kata, you will be given an array of unique elements, and your task is to rerrange the values so that the first max value is followed by the first minimum, followed by second max value then second min value, etc.
+# For example:
+# solve([15,11,10,7,12]) = [15,7,12,10,11]
+# The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
+# More examples in the test cases.
+
+def solve(arr):
+    arr = sorted(arr)
+    a = []
+    for i in range(len(arr)):
+        a.append(arr[len(arr)-1-i])
+        a.append(arr[i])
+    return a[:len(arr)]
+    return a[:len(arr)]
