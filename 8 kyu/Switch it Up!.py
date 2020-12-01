070be4fdcbd99ff53d87cfdab9ac5e8469b3289d
Rita Sergienko <72668824+Margarita-Sergienko@users.noreply.github.com>
2020-11-30 20:33:44 -0800

    add  Switch it Up!

---
+++ 8 kyu/Switch it Up!.py
@@ -0,0 +1,29 @@
+# 8 kuy
+# Switch it Up!
+# https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+# When provided with a number between 0-9, return it in words.
+# Input :: 1
+# Output :: "One".
+# If your language supports it, try using a switch statement.
+
+def switch_it_up(number):
+    if number == 0:
+        return "Zero"
+    elif number == 1:
+        return "One"
+    elif number == 2:
+        return "Two"
+    elif number == 3:
+        return "Three"
+    elif number == 4:
+        return "Four"
+    elif number == 5:
+        return "Five"
+    elif number == 6:
+        return "Six"
+    elif number == 7:
+        return "Seven"
+    elif number == 8:
+        return "Eight"
+    elif number == 9:
+        return "Nine"
